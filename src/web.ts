f0a452305c2edafa31a1e4430e1b40476c3649b7
cruxcode <swaroopshyam0@gmail.com>
2021-07-21 23:59:44 -0400

    changed web path

--- src/web.ts
+++ src/web.ts
@@ -8,11 +8,11 @@ export const web = (): Router => {
 	const router = Router();
 	const WEB_BASE = argv.web || "/storage/proj/ss6146/cruxdemoserver";
 	router.use(
-		express.static(path.resolve(WEB_BASE, "scriptsdemo-web", "dist"))
+		express.static(path.resolve(WEB_BASE, "scriptsdemo-web", "build"))
 	);
 	router.get("/", (req, res) => {
 		res.sendFile(
-			path.resolve(WEB_BASE, "scriptsdemo-web", "dist", "index.html")
+			path.resolve(WEB_BASE, "scriptsdemo-web", "build", "index.html")
 		);
 	});
 	return router;
