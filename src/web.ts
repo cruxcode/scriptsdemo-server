d11e0e356a380d78615e4adf5046632b3353e9f5
cruxcode <swaroopshyam0@gmail.com>
2021-07-21 23:57:25 -0400

    implemented web

--- src/web.ts
+++ src/web.ts
@@ -1,6 +1,19 @@
-import { Router } from "express";
+import express, { Router } from "express";
+import path from "path";
+import yargs from "yargs/yargs";
+import { hideBin } from "yargs/helpers";
+const argv = yargs(hideBin(process.argv)).argv as any;
 
 export const web = (): Router => {
 	const router = Router();
+	const WEB_BASE = argv.web || "/storage/proj/ss6146/cruxdemoserver";
+	router.use(
+		express.static(path.resolve(WEB_BASE, "scriptsdemo-web", "dist"))
+	);
+	router.get("/", (req, res) => {
+		res.sendFile(
+			path.resolve(WEB_BASE, "scriptsdemo-web", "dist", "index.html")
+		);
+	});
 	return router;
 };
