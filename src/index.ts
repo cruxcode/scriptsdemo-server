e86ea7d3e7742fecc0f1963da519efcb49eb17e4
cruxcode <swaroopshyam0@gmail.com>
2021-07-21 21:57:26 -0400

    middlewaure

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,10 @@ const app = express();
 const argv = yargs(hideBin(process.argv)).argv as any;
 const port = argv.port || 8080;
 
+app.use((req, res, next) => {
+	console.log("req arrived");
+	next();
+});
 app.use("/search", search());
 app.use("/doc", doc());
 app.use("/", web());
