fa2dc6dddd545a79c5eb47f49071f5f6498356fe
cruxcode <swaroopshyam0@gmail.com>
2021-07-21 23:03:58 -0400

    summarizer added

--- src/search.ts
+++ src/search.ts
@@ -1,5 +1,15 @@
 import express, { Router } from "express";
 import http from "http";
+import yargs from "yargs/yargs";
+import { hideBin } from "yargs/helpers";
+import path from "path";
+import fs from "fs";
+import { uuid } from "uuidv4";
+import { spawn } from "child_process";
+
+const argv = yargs(hideBin(process.argv)).argv as any;
+const CLIR_OUTPUT_BASE = argv.clir || "/storage/proj/ss6146/cruxdemoserver";
+const procs = argv.procs || 2;
 
 export const search = (): Router => {
 	const router = Router();
@@ -7,11 +17,14 @@ export const search = (): Router => {
 		const query = req.body.query;
 		const source = req.body.source;
 		const size = req.body.size;
-
+		const lang = req.body.lang;
 		// 1 - guards
-		if (!query || !source || !size) {
+		if (!query || !source || !size || !lang) {
 			res.status(400);
-			res.send({ success: false, msg: "query is required" });
+			res.send({
+				success: false,
+				msg: "query, source, size, lang are required",
+			});
 			return;
 		}
 
@@ -32,14 +45,40 @@ export const search = (): Router => {
 				});
 				response.on("end", () => {
 					const result = chunks.join();
-					res.send({ success: true, result });
 					// 3 - write response to a file
-
+					const clir_output_filename = uuid() + ".json";
+					const clir_output_path = path.resolve(
+						CLIR_OUTPUT_BASE,
+						lang,
+						source
+					);
+					fs.writeFileSync(
+						path.resolve(clir_output_path, clir_output_filename),
+						result
+					);
 					// 4 - call summarizer
+					const docker = spawn(
+						`docker exec summ_${lang}_${source} summarize_queries_demo ${clir_output_filename} ${procs}`
+					);
+					docker.stdout.on("data", (data) => {
+						console.log(`stdout: ${data}`);
+					});
 
+					docker.stderr.on("data", (data) => {
+						console.log(`stderr: ${data}`);
+					});
+
+					docker.on("error", (error) => {
+						console.log(`error: ${error.message}`);
+					});
+
+					docker.on("close", (code) => {
+						console.log(`child process exited with code ${code}`);
+					});
 					// 5 - wait for atleast one summary
 
 					// 6 - send res
+					res.send({ success: true, result, clir_output_filename });
 				});
 				response.on("error", (err) => {
 					console.log(err);
